dfadbac88754874f1f88e4d5997711a3b2a8e305
marsel945 <rewoherson519@gmail.com>
2023-09-29 07:51:40 +0800

    mempercantik halaman profile

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,11 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @yield('customCss')
+
 </head>
+
 
 <body>
 
